23c2ea9fcadb318566ca0b2f501ca758006e689c
thelonewolf316 <thelonewolf316>
2019-02-07 21:41:29 -0500

    setup c o g s

--- bot.py
+++ bot.py
@@ -1,18 +1,44 @@
 from discord.ext import commands
-import discord, tokens, constants
+import discord, tokens, os
 
-Client = discord.Client()
-client = commands.Bot(command_prefix="")
+#Loading all cog files from /cogs
+extensions=[]
+for cogfile in os.listdir("cogs"):
+    if str(cogfile).endswith(".py"):
+        extensions.append(cogfile[:-3])
 
+
+client = commands.Bot(command_prefix='!', case_insenghjgjsitive=True)
+
+#On connection
 @client.event
 async def on_ready():
-    print("Bot is online and connected to Discord")  # When Bot Connects
+    print('Bot is online and connected to Discord') 
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!'):
-        constants.run_coro(client.send_message(message.channel, "test"), client)
+#Load and unload cog commands
+@client.command()
+async def load(extensions):
+    try:
+        client.load_extension(extension)
+        print('Loaded', extension)
+    except Exception as error:
+        print('Cannot load:', extension, error)
 
+@client.command()
+async def unload(extensions):
+    try:
+        client.unload_extension(extension)
+        print('Unloaded', extension)
+    except Exception as error:
+        print('Cannot unload:', extension, error)
+
+#Importing/enabling cogs from extensions list
+if __name__ == '__main__':
+    for extension in extensions:
+        try:
+            client.load_extension("cogs."+extension)
+        except Exception as error:
+            print('Cannot load:', extension, error)
 
 client.run(tokens.discord)
 
--- constants.py
+++
@@ -1,4 +0,0 @@
-import asyncio
-
-def run_coro(coro, client):
-    fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
