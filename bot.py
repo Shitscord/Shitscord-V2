ddd2b85b6776ef916d3db16d39e1550996b72356
thelonewolf316 <thelonewolf316>
2019-02-08 19:31:26 -0500

    removed loading and unloading function commands because that was just uneccessary

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ for cogfile in os.listdir("cogs"):
     if str(cogfile).endswith(".py"):
         extensions.append(cogfile[:-3])
 
-
 client = commands.Bot(command_prefix='!')
 testvar=1
 
@@ -15,23 +14,6 @@ testvar=1
 @client.event
 async def on_ready():
     print('Bot is online and connected to Discord') 
-
-#Load and unload cog commands
-@client.command()
-async def load(extensions):
-    try:
-        client.load_extension(extension)
-        print('Loaded', extension)
-    except Exception as error:
-        print('Cannot load:', extension, error)
-
-@client.command()
-async def unload(extensions):
-    try:
-        client.unload_extension(extension)
-        print('Unloaded', extension)
-    except Exception as error:
-        print('Cannot unload:', extension, error)
 
 #Importing/enabling cogs from extensions list
 if __name__ == '__main__':
