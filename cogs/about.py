462d06f9acaade2baa80c709d5bf10c3df85c1a7
thewonelolf <thewonelolf@github>
2020-05-23 17:23:26 -0400

    fixed send on about

--- cogs/about.py
+++ cogs/about.py
@@ -7,7 +7,7 @@ class About(commands.Cog):
 
     @commands.command(pass_context=True)
     async def about(self, ctx):
-        await ctx.send("Shitscord v2, Developmental Version.\nCreated by TheWoneLolf. Use !help for commands.\nhttps://github.com/Shitscord/Shitscord-v2", os.getenv("HEROKU_RELEASE_VERSION"))
+        await ctx.send("Shitscord v2, Developmental Version.\nCreated by TheWoneLolf. Use !help for commands.\nhttps://github.com/Shitscord/Shitscord-v2" + str(os.getenv("HEROKU_RELEASE_VERSION")))
         
 def setup(client):
     client.add_cog(About(client))
