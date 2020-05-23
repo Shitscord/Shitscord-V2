c3dda299bffcafc855449e767f456402e08194ae
thewonelolf <thewonelolf@github>
2020-05-23 17:19:39 -0400

    Testing heroku vars

--- cogs/about.py
+++ cogs/about.py
@@ -1,4 +1,4 @@
-import discord
+import discord, os
 from discord.ext import commands
 
 class About(commands.Cog):
@@ -7,7 +7,7 @@ class About(commands.Cog):
 
     @commands.command(pass_context=True)
     async def about(self, ctx):
-        await ctx.send("Shitscord v2, Developmental Version.\nCreated by TheWoneLolf. Use !help for commands.\nhttps://github.com/Shitscord/Shitscord-v2")
+        await ctx.send("Shitscord v2, Developmental Version.\nCreated by TheWoneLolf. Use !help for commands.\nhttps://github.com/Shitscord/Shitscord-v2", os.getenv("HEROKU_RELEASE_VERSION"))
         
 def setup(client):
     client.add_cog(About(client))
